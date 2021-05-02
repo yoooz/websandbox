ff61586c33e5dbb38e039cc0302602516191fe7f
yoooz <oor.9@outlook.com>
2021-05-03 01:50:51 +0900

    create dropdown component

---
+++ frontend/src/components/MyDropdown.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="text-center ma-2">
+    <v-menu offset-y>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn color="primary" dark v-bind="attrs" v-on="on"> Dropdown </v-btn>
+      </template>
+      <v-list>
+        <v-list-item v-for="(item, index) in items" :key="index">
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </div>
+</template>
+
+<script lant="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  data: () => ({
+    items: [
+      { title: "Click Me" },
+      { title: "Click Me" },
+      { title: "Click Me" },
+      { title: "Click Me" },
+      { title: "Click Me" },
+    ],
+  }),
+});
+</script>
