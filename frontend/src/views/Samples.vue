241c6ef176b366b18ffa5a0bfd829691038d9db7
yoooz <oor.9@outlook.com>
2021-05-03 01:50:56 +0900

    menu

--- frontend/src/views/Samples.vue
+++ frontend/src/views/Samples.vue
@@ -1,9 +1,22 @@
 <template>
   <div class="samples">
     <h1>This is an samples page</h1>
-    <MyDialog></MyDialog>
-    <MyBottomSheet></MyBottomSheet>
-    <MySnackbar></MySnackbar>
+    <div class="sample-container">
+      <div class="fruits-list">
+        <v-list>
+          <v-list-item v-for="(item, index) in fruits" :key="index">
+            <v-list-item-title>{{ item.name }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </div>
+
+      <div class="buttons">
+        <MyDialog></MyDialog>
+        <MyBottomSheet></MyBottomSheet>
+        <MySnackbar></MySnackbar>
+        <MyDropdown></MyDropdown>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,13 +25,24 @@ import Vue from "vue";
 import MyDialog from "@/components/MyDialog.vue";
 import MyBottomSheet from "@/components/MyBottomSheet.vue";
 import MySnackbar from "@/components/MySnackbar.vue";
+import MyDropdown from "@/components/MyDropdown.vue";
 
 export default Vue.extend({
   components: {
     MyDialog,
     MyBottomSheet,
     MySnackbar,
+    MyDropdown,
   },
+  data: () => ({
+    fruits: [
+      { name: "Apple" },
+      { name: "Orange" },
+      { name: "Grape" },
+      { name: "Cherry" },
+      { name: "Blueberry" },
+    ],
+  }),
 });
 </script>
 
@@ -26,5 +50,18 @@ export default Vue.extend({
 div.samples {
   display: flex;
   flex-direction: column;
+}
+
+div.sample-container {
+  display: flex;
+}
+
+div.fruits-list {
+  display: inline-block;
+}
+
+div.buttons {
+  flex-grow: 2;
+  display: inline-block;
 }
 </style>
