0611eaf323f76002cea9e0d93586e6521eb5a53f
yoooz <oor.9@outlook.com>
2021-05-02 20:56:06 +0900

    create snackbar component

---
+++ frontend/src/components/MySnackbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="text-center ma-2">
+    <v-btn dark @click="snackbar = true"> Open </v-btn>
+    <v-snackbar v-model="snackbar">
+      {{ text }}
+      <template v-slot:action="{ attrs }">
+        <v-btn color="pink" text v-bind="attrs" @click="snackbar = false">
+          close
+        </v-btn>
+      </template>
+    </v-snackbar>
+  </div>
+</template>
+
+<script lant="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  data() {
+    return {
+      snackbar: false,
+      text: "this is snackbar",
+    };
+  },
+});
+</script>
--- frontend/src/views/Samples.vue
+++ frontend/src/views/Samples.vue
@@ -3,6 +3,7 @@
     <h1>This is an samples page</h1>
     <MyDialog></MyDialog>
     <MyBottomSheet></MyBottomSheet>
+    <MySnackbar></MySnackbar>
   </div>
 </template>
 
@@ -10,11 +11,13 @@
 import Vue from "vue";
 import MyDialog from "@/components/MyDialog.vue";
 import MyBottomSheet from "@/components/MyBottomSheet.vue";
+import MySnackbar from "@/components/MySnackbar.vue";
 
 export default Vue.extend({
   components: {
     MyDialog,
     MyBottomSheet,
+    MySnackbar,
   },
 });
 </script>
