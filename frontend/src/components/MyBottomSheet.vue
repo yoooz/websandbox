c39385402a1511557e6ea658ef380c6cc3b29cda
yoooz <oor.9@outlook.com>
2021-05-02 20:47:44 +0900

    create bottom sheet component

---
+++ frontend/src/components/MyBottomSheet.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-bottom-sheet v-model="sheet" persistent>
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn color="green" dark v-bind="attrs" v-on="on"> Open </v-btn>
+    </template>
+    <v-sheet class="text-center" height="200px">
+      <v-btn class="mt-6" text color="error" @click="sheet = !sheet">
+        close
+      </v-btn>
+      <div class="py-3">This is a bottom sheet</div>
+    </v-sheet>
+  </v-bottom-sheet>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  data() {
+    return {
+      sheet: false,
+    };
+  },
+});
+</script>
--- frontend/src/views/Samples.vue
+++ frontend/src/views/Samples.vue
@@ -2,16 +2,19 @@
   <div class="samples">
     <h1>This is an samples page</h1>
     <MyDialog></MyDialog>
+    <MyBottomSheet></MyBottomSheet>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import MyDialog from "@/components/MyDialog.vue";
+import MyBottomSheet from "@/components/MyBottomSheet.vue";
 
 export default Vue.extend({
   components: {
     MyDialog,
+    MyBottomSheet,
   },
 });
 </script>
