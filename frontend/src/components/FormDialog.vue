cdb81eb5d3d959fb1fdaf501fbc0a21021bc37af
yoooz <oor.9@outlook.com>
2021-05-04 16:16:55 +0900

    create new memo form

---
+++ frontend/src/components/FormDialog.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="text-center ma-2">
+    <v-dialog
+      transition="dialog-bottom-transition"
+      max-width="600"
+      v-model="dialog"
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn elevation="2" rounded color="primary" v-bind="attrs" v-on="on">
+          Create
+        </v-btn>
+      </template>
+      <v-card>
+        <v-card-title class="headline grey lighten-2">
+          Create New Memo
+        </v-card-title>
+
+        <v-form>
+          <v-container>
+            <v-row>
+              <v-col>
+                <v-text-field label="title" v-model="title"></v-text-field>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col>
+                <v-textarea label="content" v-model="content"></v-textarea>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-form>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="secondary" text @click="dialog = false">close</v-btn>
+          <v-btn color="primary" text @click="submit"> submit </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import axios from "axios";
+
+type Data = {
+  dialog: boolean;
+  title: string;
+  content: string;
+};
+
+export default Vue.extend({
+  data(): Data {
+    return {
+      dialog: false,
+      title: "",
+      content: "",
+    };
+  },
+  methods: {
+    submit() {
+      const params = new URLSearchParams();
+      params.append("title", this.title);
+      params.append("content", this.content);
+
+      axios.post("/memo/add", params).then((response) => {
+        console.dir(response);
+        this.dialog = false;
+      });
+    },
+  },
+});
+</script>
--- frontend/src/views/Form.vue
+++ frontend/src/views/Form.vue
@@ -1,12 +1,19 @@
 <template>
-  <div class="forms"><v-btn @click="onClickButton">button</v-btn></div>
+  <div class="forms">
+    <v-btn @click="onClickButton">button</v-btn>
+    <FormDialog></FormDialog>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import axios from "axios";
+import FormDialog from "@/components/FormDialog.vue";
 
 export default Vue.extend({
+  components: {
+    FormDialog,
+  },
   methods: {
     onClickButton() {
       console.log("clicked");
