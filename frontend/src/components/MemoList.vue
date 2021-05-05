40094f05bf3ac85556348a7285c4f7496f930442
yoooz <oor.9@outlook.com>
2021-05-05 17:29:14 +0900

    implement update & delete

--- frontend/src/components/MemoList.vue
+++ frontend/src/components/MemoList.vue
@@ -1,6 +1,6 @@
 <template>
   <v-card class="d-flex" height="70vh">
-    <v-list width="200" style="overflow-y: scroll">
+    <v-list width="200" class="title-list">
       <v-list-item
         v-for="(item, index) in items"
         :key="index"
@@ -13,7 +13,13 @@
       </v-list-item>
     </v-list>
     <v-divider vertical></v-divider>
-    <div class="content" style="overflow-y: scroll">{{ currentContent }}</div>
+    <div v-show="currentId != -1" class="content">
+      <v-subheader>
+        <v-btn @click="onClickUpdate"> 更新 </v-btn>
+        <v-btn @click="onClickDelete"> 削除 </v-btn>
+      </v-subheader>
+      <v-textarea v-model="currentContent"></v-textarea>
+    </div>
   </v-card>
 </template>
 
@@ -31,12 +37,29 @@ type Item = {
 export default Vue.extend({
   data: () => ({
     items: [],
+    currentId: -1,
     currentContent: "",
   }),
   methods: {
     onClickListItem(item: Item) {
-      console.log(item.content);
+      console.dir(item);
+      this.currentId = item.id;
       this.currentContent = item.content;
+    },
+    onClickUpdate() {
+      const params = new URLSearchParams();
+      params.append("id", this.currentId);
+      params.append("content", this.currentContent);
+      axios.post("/memo/update", params).then((response) => {
+        location.reload();
+      });
+    },
+    onClickDelete() {
+      const params = new URLSearchParams();
+      params.append("id", this.currentId);
+      axios.post("/memo/delete", params).then((response) => {
+        location.reload();
+      });
     },
   },
   mounted: function () {
@@ -53,5 +76,14 @@ div.content {
   flex-grow: 2;
   text-align: left;
   padding: 10px;
+}
+
+div.title-list {
+  overflow-y: scroll;
+}
+div.content {
+  overflow-y: scroll;
+  display: flex;
+  flex-direction: column;
 }
 </style>
